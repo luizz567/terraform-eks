74367c51f824e25936ccd6bc7dcff48e5ce5d229
root <root@Luiz.localdomain>
2025-08-07 00:05:38 -0300

    feat: add vpc and remote backend

--- provider.tf
+++ provider.tf
@@ -1,12 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "6.7.0"
     }
   }
+  backend "s3" {
+    bucket = "comunidadedevops-terraform-ektes"
+    key    = "development/terraform.tfstate"
+    region = "us-east-1"
+  }
 }
-
 provider "aws" {
   region = "us-east-1"
 }
+
