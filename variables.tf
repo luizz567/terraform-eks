456ba490079b191ab989c6267b89b1a145d4ea13
luizz567 <martins.567@hotmail.com>
2025-08-11 20:55:05 -0300

    feat: creat project_name var

--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "cidr_block" {
   type        = string
   description = "Network CIDR block for the VPC"
 }
+
+variable "project_name" {
+  type        = string
+  description = "Project name to be used to tag resources"
+
+}
